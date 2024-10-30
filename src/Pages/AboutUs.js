cd09b78cc01393b19625875fef1af220758cc566
BismaRafique7 <fahadrafique92@gmail.com>
2024-10-31 01:20:22 +0500

    the first trial

---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../Components/Footer';
+import Headar from '../Components/Headar'; 
+
+function AboutUs() {
+  return (
+    <div>
+      <Headar /> 
+      <p>About Us</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default AboutUs;
---
+++ src/Pages/ContactUs.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../Components/Footer';
+import Headar from '../Components/Headar';
+
+function ContactUs() {
+  return (
+    <div>
+        <Headar /> 
+       ContactUs
+        <Footer />
+    </div>
+  )
+}
+
+export default ContactUs
